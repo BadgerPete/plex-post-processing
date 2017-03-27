4ff3fcaca37cdf825c70955afe68f7f5631fbfac
Peter Boguszewski <pete@uwalumni.com>
2017-03-27 08:47:07 -0500

    Inital stab at bash processing

---
+++ plexProcessing.sh
@@ -0,0 +1,33 @@
+#!/usr/local/bin/bash
+# Add bash path to Plex path.
+#set path = ($path /usr/local/bin)
+
+# Sleep for a pseudorandom period (up to 10 seconds) to limit the number of instances that start at once
+# $$ = process % = modulo
+sleep `echo $$%10 | bc`
+now = `date`
+starttime = `date +%s`
+
+filedirname=$(dirname "$1")
+echo "filedirname: ${filedirname}" >> /tmp/comchap.log
+
+fullfilename=$(basename "$1")
+echo "fullfilename: ${fullfilename}"
+
+extensioname="${fullfilename##*.}"
+echo "extensioname: ${extensioname}" >> /tmp/comchap.log 
+
+filename="${fullfilename%.*}"
+echo "filename: ${filename}" >> /tmp/comchap.log 
+
+echo "Processing: $now $1" >> /tmp/comchap.log
+# Everything betwen here and the next # needs to be on one line! Make sure there's no linewrap.
+/usr/local/comskiplinux/comchap/comcut --ffmpeg=/usr/local/bin/ffmpeg --comskip=/usr/local/comskiplinux/Comskip-master/comskip --lockfile=/tmp/comchap.lock --comskip-ini=/usr/local/comskiplinux/Comskip-master/comskip.ini "$1" "/tmp/${fullfilename}"
+
+# Re-encode as an mp4 file so we keep the original
+ffmpeg -i "/tmp/${fullfilename}" -acodec copy -vcodec copy "${filedirname}/${filename}.mp4"
+
+#
+endtime = `date +%s`
+@ timediff = $endtime - $starttime
+echo "Complete:   $timediff seconds $1" >> /tmp/comchap.log
